7f23f4580c081e673f3343152f2aa564327c7b9a
SushantPatil861 <sushantpatil861@gmail.com>
2025-06-11 06:16:58 +0530

    updated docker scripts

--- task-mgmt-backend/docker-build-run.sh
+++ task-mgmt-backend/docker-build-run.sh
@@ -1,5 +1,3 @@
-./mvnw clean package -DskipTests
-
-docker build -t sushantpatil734/task-mgmt:latest
+docker buildx build --no-cache -t sushantpatil734/task-mgmt:latest .
 
 docker run -p 8090:8090 sushantpatil734/task-mgmt:latest
